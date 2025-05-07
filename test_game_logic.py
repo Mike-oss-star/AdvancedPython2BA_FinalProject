cf9ca6cedadceb950d7f8b1c540436a45491f2e1
Mike-oss-star <23390@ecam.be>
2025-05-08 00:11:48 +0200

    text_game_logic

--- test_game_logic.py
+++ test_game_logic.py
@@ -1,25 +1,25 @@
 import pytest
-from game_logic import isWinning, evaluate, next_State
+from game_logic import isWinning, evaluate, next_State, get_available_pieces, get_available_positions
 
 def test_isWinning_true():
-    board = ['BDPC'] * 4 + [None] * 12
+    board = ['BDFP'] * 4 + [None] * 12
     assert isWinning(board) is True
 
 def test_isWinning_false():
-    board = ['BDPC', 'SDPC', None, 'LDPC'] + [None] * 12
+    board = ['BDFP', 'SDEP', None, 'SDEC'] + [None] * 12
     assert isWinning(board) is False
 
 def test_next_state():
     state = {
         "board": [None] * 16,
-        "piece": "BDPC",
+        "piece": "BDFP",
         "current": 0
     }
-    move = {"pos": 0, "piece": "SDPC"}
+    move = {"pos": 0, "piece": "SDEC"}
     new_state = next_State(state, move)
 
-    assert new_state["board"][0] == "BDPC"
-    assert new_state["piece"] == "SDPC"
+    assert new_state["board"][0] == "BDFP"
+    assert new_state["piece"] == "SDEC"
     assert new_state["current"] == 1
 
 def test_evaluate_returns_int():
@@ -30,3 +30,21 @@ def test_evaluate_returns_int():
     }
     score = evaluate(state, 0)
     assert isinstance(score, int)
+
+def test_get_available_pieces():
+    state={
+        "board": [None] * 16,
+        "piece": "BDFP",
+        "current": 0
+    }
+    pieces = get_available_pieces(state)
+    assert len(pieces)==15
+
+def test_get_available_positions():
+    state={
+        "board": [None] * 16,
+        "piece": "BDFP",
+        "current": 0
+    }
+    positions=get_available_positions(state)
+    assert len(positions)==16
